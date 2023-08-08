07eb83fc3b77960192aba9985b5888a7639ac59c
Luca Sahli <lucaandrea.sahli@gmail.com>
2023-08-08 10:42:13 +0200

    Before Docker

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.json({ hello: "world"})
+})
+
+const port = Number(process.env.PORT || 8080) 
+app.listen(port, "0.0.0.0", () => {
+    console.log(`Server listenging at http://localhost:${port}`)
+})
+
 console.log("Hello World")
