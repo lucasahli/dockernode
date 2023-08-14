3d593a56599f3db48fb21e81f271b8bea78d47da
Luca Sahli <lucaandrea.sahli@gmail.com>
2023-08-14 16:11:37 +0200

    TESTS RUN and PASS

---
+++ jest.config.cjs
@@ -0,0 +1,71 @@
+// module.exports = {
+//     extensionsToTreatAsEsm: ['.ts'],
+//     moduleNameMapper: {
+//       '^(\\.{1,2}/.*)\\.js$': '$1',
+//     },
+//     transform: {'^.+\\.ts?$': ['ts-jest', { useEsm: true }]},
+//     testEnvironment: 'node',
+//     testRegex: '/tests/.*\\.(test|spec)?\\.(ts|tsx)$',
+//     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+//   };
+
+  module.exports ={
+        roots: [
+            "<rootDir>",
+            "<rootDir>/tests",
+        ],
+        testMatch: [
+            "**/__tests__/**/*.+(ts|tsx)",
+            "**/?(*.)+(spec|test).+(ts|tsx)"
+        ],
+        transform: {
+            "^.+\\.(t|j)sx?$": [
+                "@swc/jest",
+                {
+                    jsc: {
+                        target: "es2022",
+                    },
+                },
+            ],
+        },
+        testEnvironment: 'node',
+        modulePaths: ["<rootDir>", "<rootDir>/tests", "<rootDir>/src"],
+        moduleDirectories: ["node_modules"],
+        moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+        moduleNameMapper: {
+          '^(\\.{1,2}/.*)\\.js$': '$1',
+        },
+        transformIgnorePatterns: ['node_modules/'],
+        extensionsToTreatAsEsm: ['.ts', '.tsx'],
+    };
+
+// import type {Config} from 'jest';
+
+// const config: Config = {
+//     roots: [
+//         "<rootDir>",
+//         "<rootDir>/tests",
+//     ],
+//     testMatch: [
+//         "**/__tests__/**/*.+(ts|tsx)",
+//         "**/?(*.)+(spec|test).+(ts|tsx)"
+//     ],
+//     transform: {
+//         "^.+\\.(t|j)sx?$": [
+//             "@swc/jest",
+//             {
+//                 jsc: {
+//                     target: "es2022",
+//                 },
+//             },
+//         ],
+//     },
+//     testEnvironment: 'node',
+//     modulePaths: ["<rootDir>", "<rootDir>/tests", "<rootDir>/src"],
+//     moduleDirectories: ["node_modules"],
+//     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+//     transformIgnorePatterns: ['node_modules/'],
+//     extensionsToTreatAsEsm: ['.ts', '.tsx'],
+// };
+
+// export default config;
