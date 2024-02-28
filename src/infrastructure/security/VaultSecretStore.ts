1c6ce7d5ec0fc28f2cffbd8d82933f0f4f0f6e20
Luca <32506940+lucasahli@users.noreply.github.com>
2024-02-28 12:19:12 +0100

    removed vault from typescript code

--- src/infrastructure/security/VaultSecretStore.ts
+++ src/infrastructure/security/VaultSecretStore.ts
@@ -1,5 +1,5 @@
 import {SecretStore} from "../../core/portsAndInterfaces/interfaces/SecretStore.js";
-import vault from 'node-vault';
+// import vault from 'node-vault';
 
 
 
@@ -11,11 +11,11 @@ export class VaultSecretStore implements SecretStore {
             throw new Error('HCP_API_TOKEN is not set');
         }
     }
-    private vaultClient = vault({
-        apiVersion: 'v1',
-        endpoint: 'https://api.cloud.hashicorp.com/secrets/2023-06-13/organizations/0cf8f694-8355-416b-a72d-e7efba19b7b2/projects/2bdc7259-f750-4523-bf43-9a02a08decbc/apps/reminder-app/open', // Vault server URL
-        token: `${process.env.HCP_API_TOKEN}`, // Vault token
-    });
+    // private vaultClient = vault({
+    //     apiVersion: 'v1',
+    //     endpoint: 'https://api.cloud.hashicorp.com/secrets/2023-06-13/organizations/0cf8f694-8355-416b-a72d-e7efba19b7b2/projects/2bdc7259-f750-4523-bf43-9a02a08decbc/apps/reminder-app/open', // Vault server URL
+    //     token: `${process.env.HCP_API_TOKEN}`, // Vault token
+    // });
 
     readSecret(secretAlias: string): Promise<string> {
         this.getSecrets();
@@ -24,8 +24,8 @@ export class VaultSecretStore implements SecretStore {
 
     private async getSecrets() {
         try {
-            const secretData = await this.vaultClient.read('secret/myproject');
-            console.log(secretData.data);
+            // const secretData = await this.vaultClient.read('secret/myproject');
+            console.log("secretData.data");
             // Use secretData.data to access your secrets
         } catch (error) {
             console.error('Error reading from Vault:', error);
