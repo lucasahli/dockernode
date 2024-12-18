0bf66489132eb2d4a02107bb7e29f967d2c7ddcd
Luca <32506940+lucasahli@users.noreply.github.com>
2024-12-18 10:54:39 +0100

    changed: enambled iam in tf

--- main.tf
+++ main.tf
@@ -58,6 +58,13 @@ terraform {
   }
 }
 
+resource "google_project_service" "enable_iam_api" {
+  project = var.project_id
+  service = "iam.googleapis.com"
+  disable_on_destroy = false
+}
+
+
 resource "google_project_service" "iam_api" {
   service            = "iam.googleapis.com"
   disable_on_destroy = false
